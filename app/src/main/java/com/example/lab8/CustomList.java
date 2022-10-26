2d92b6b121244471e59034ce337b6686f0c9bdfb
Vedant Vyas <53002423+vedudx@users.noreply.github.com>
2022-10-26 17:55:28 -0600

    HasCity

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -61,4 +61,5 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,8 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -28,4 +30,18 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);}
+
+
+    @Test
+    public void testHasCity() {
+        list = MockCityList();
+
+        City city = new City("Estevan", "SK");
+        list.addCity(city);
+
+        assertTrue(city.hasCity(city));
+
+        // test for a city which isn't there
+        assertFalse(cityList.hasCity(new City("testCity", "testProvince")));
+    }
 }
